843ea9153539a562e0a54d851c247d7a0acbf97f
sujithkarthika <ammu2713858>
2014-04-19 16:38:15 +0530

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "sujith_test",
+    "version" : "1.0",
+    "depends" : ['account_voucher'],
+    "author" : "Novasoft Consultancy Services Pvt. Ltd.",
+    'category' : 'Generic Modules/Others',
+    "description": """ Sujith - Management Module
+    
+    """,
+    'website': 'http://www.novasoftindia.com',
+    'data': ['test.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
